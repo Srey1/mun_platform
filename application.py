0e7a01bd3da6fc923fa68406f48a78a67ca1d865
Srey <emailsreyas@gmail.com>
2021-02-11 09:09:02 +0100

    pushing to heroku

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ import os
 
 
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
-#from flask_session import Session
+from flask_session import Session
 #from tempfile import mkdtemp
 #from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -251,7 +251,7 @@ def login():
            # return apology("invalid", 403)
 
         # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        session["user_id"] = rows.id
 
         return redirect("/")
 
