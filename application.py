d5638f4249138b9f07bbc8132aecf294d1bfd31c
Srey <emailsreyas@gmail.com>
2021-02-14 17:14:00 +0100

    pushing to heroku

--- application.py
+++ application.py
@@ -22,7 +22,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
-Session(app)
+#Session(app)
 #login_manager.init_app(app)
 
 app.permanent_session_lifetime = timedelta(days=1)
