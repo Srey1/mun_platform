76bccb35248e1a71937a072fa8073e2662b6a43a
Srey <emailsreyas@gmail.com>
2021-02-14 17:26:32 +0100

    pushing to heroku

--- application.py
+++ application.py
@@ -31,6 +31,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80))
+    hand = db.Column(db.String(80))
 
 
 
